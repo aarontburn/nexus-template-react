480fd413d22a1d226ef2719d23e8ef1cc9272c3c
Aaron <aarontburnham@hotmail.com>
2025-03-31 00:34:12 -0700

    removed scoping

--- src/developer.Sample_Module/{SampleModule}Renderer.ts
+++ src/developer.Sample_Module/{SampleModule}Renderer.ts
@@ -1,70 +1,65 @@
+
 /**
- * The renderer. Wrapped in an anonymous function for scoping.
+ *  The ID of the associated module. Must match the process.
  */
-(() => {
+const MODULE_ID: string = "developer.Sample_Module";
 
-    /**
-     *  The ID of the associated module. Must match the process.
-     */
-    const MODULE_ID: string = "developer.Sample_Module";
+/**
+ *  Sends information to the the process.
+ * 
+ *  @param eventType    The name of the event.
+ *  @param data         Any data to send.
+ */
+function sendToProcess(eventType: string, data: any = []): Promise<any> {
+    return window.parent.ipc.send(MODULE_ID, eventType, ...data);
+}
 
-    /**
-     *  Sends information to the the process.
-     * 
-     *  @param eventType    The name of the event.
-     *  @param data         Any data to send.
-     */
-    function sendToProcess(eventType: string, data: any = []): Promise<any> {
-        return window.parent.ipc.send(MODULE_ID, eventType, ...data);
-    }
+const iframe: HTMLIFrameElement = document.getElementById('react-iframe') as HTMLIFrameElement;
+if (window.parent.common.args.includes("--dev")) {
+    // Or change this to wherever the react webserver is hosted.
+    iframe.src = "http://localhost:5173/"
+}
 
-    const iframe: HTMLIFrameElement = document.getElementById('react-iframe') as HTMLIFrameElement;
-    if (window.parent.common.args.includes("--dev")) {
-        // Or change this to wherever the react webserver is hosted.
-        iframe.src = "http://localhost:5173/"
-    }
+function sendToIFrame(eventType: string, data: any = []) {
+    iframe.contentWindow.postMessage({ eventType: eventType, data: data }, "*");
+}
 
-    function sendToIFrame(eventType: string, data: any = []) {
-        iframe.contentWindow.postMessage({ eventType: eventType, data: data }, "*");
-    }
-
-    /**
-     *  Handle events from the process.
-     * 
-     *  In a react context, simply passes the message to the react window.
-     */
-    window.parent.ipc.on(MODULE_ID, async (_, eventType: string, data: any = []) => {
-        sendToIFrame(eventType, data);
-    });
+/**
+ *  Handle events from the process.
+ * 
+ *  In a react context, simply passes the message to the react window.
+ */
+window.parent.ipc.on(MODULE_ID, async (_, eventType: string, data: any = []) => {
+    sendToIFrame(eventType, data);
+});
 
 
 
-    /**
-     *  React only: Listen to events from the react renderer and passes it to the process.
-     */
-    window.addEventListener("message", (event) => {
-        sendToProcess(event.data.eventType, event.data.data)
-    });
+/**
+ *  React only: Listen to events from the react renderer and passes it to the process.
+ */
+window.addEventListener("message", (event) => {
+    sendToProcess(event.data.eventType, event.data.data)
+});
 
 
-    let accentColor: string | undefined = "";
-    const observer = new MutationObserver((mutations: MutationRecord[]) => {
-        mutations.forEach((mutationRecord: MutationRecord) => {
-            const cssString: string = (mutationRecord.target as HTMLElement).attributes.getNamedItem("style").value;
-            const splitString: string[] = cssString.split(";");
+let accentColor: string | undefined = "";
+const observer = new MutationObserver((mutations: MutationRecord[]) => {
+    mutations.forEach((mutationRecord: MutationRecord) => {
+        const cssString: string = (mutationRecord.target as HTMLElement).attributes.getNamedItem("style").value;
+        const splitString: string[] = cssString.split(";");
 
-            for (const s of splitString) {
-                if (s.includes("--accent-color")) {
-                    const value: string = s.split(" ")[1];
-                    if (value !== accentColor) {
-                        accentColor = value;
-                        sendToIFrame("accent-color-changed", value);
-                    }
-                    break;
+        for (const s of splitString) {
+            if (s.includes("--accent-color")) {
+                const value: string = s.split(" ")[1];
+                if (value !== accentColor) {
+                    accentColor = value;
+                    sendToIFrame("accent-color-changed", value);
                 }
+                break;
             }
-        });
+        }
     });
-    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['style'] })
-})();
+});
+observer.observe(document.documentElement, { attributes: true, attributeFilter: ['style'] })
 
