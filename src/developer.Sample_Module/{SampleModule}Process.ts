d2abc54fa2e5b2c9eaf60ba59f1d43e419973322
Aaron <aarontburnham@hotmail.com>
2025-04-01 00:41:50 -0700

    swapped to default

--- src/developer.Sample_Module/{SampleModule}Process.ts
+++ src/developer.Sample_Module/{SampleModule}Process.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import { IPCCallback, Process, Setting } from "@nexus/nexus-module-builder";
 
 
-export class SampleModuleProcess extends Process {
+export default class SampleModuleProcess extends Process {
 
     private static readonly MODULE_NAME: string = "Sample Module";
     private static readonly MODULE_ID: string = "developer.Sample_Module";
